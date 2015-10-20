289d10bee41b6171c3aa6fea7eaf1284bb2bfd4d
Lyndon Maydwell <lyndon@sordina.net>
2015-10-20 11:42:38 +1100

    Braces argument

--- src/Main.hs
+++ src/Main.hs
@@ -2,66 +2,91 @@
 
 module Main where
 
+import Data.List.Split
 import Text.Megaparsec
 import Text.Megaparsec.Prim
-import System.Environment
+import Control.Monad
 import System.Exit
 import System.IO
+
+import Options.Applicative hiding (some)
+import qualified Options.Applicative as A
 
 -- Main:
 
 main :: IO ()
-main = getArgs >>= go
+main = join $ execParser optsInfo
 
-go :: [String] -> IO ()
-go ("-h"    :_) = help
-go ("--help":_) = help
-go []           = getContents >>= goT "STDIN"
-go fs           = mapM_ goF fs
+-- Options:
 
-goF :: String -> IO ()
-goF f = readFile f >>= goT ("File: " ++ f)
+optsInfo :: ParserInfo (IO ())
+optsInfo = info (helper <*> opts)
+      ( fullDesc
+     <> header   "Pretty-Braces parses, and then pretty-prints strings with balanced braces."
+     <> progDesc "Also accepts input on STDIN." )
 
-goT :: String -> String -> IO ()
-goT f t = case parse (items <* eof) f t
-         of Left  err -> hPrint stderr err >> exitFailure
-            Right res -> putStr $ unlines $ pps res
+opts :: Parser (IO ())
+opts = go <$> bracesArg <*> filesArg
 
-help :: IO ()
-help = putStrLn "Usage: [STDIN |] prettybraces [-h | --help] [FILE]*"
+bracesArg :: Parser [Brackets]
+bracesArg = A.option (bracesArgParser <$> str)
+   ( long    "braces"
+  <> short   'b'
+  <> help    "Space-separated, comma-separated pairs of braces, e.g. '(,) {,} <,>'."
+  <> value   [("(",")"),("{","}"),("[","]")])
+
+bracesArgParser :: String -> [Brackets]
+bracesArgParser = concatMap (pair . splitOn ",") . words
+  where
+  pair [l,r] = [(l,r)]
+  pair _     = []
+
+filesArg :: Parser [String]
+filesArg = A.many (argument str (metavar "FILES..."))
+
+-- Driver:
+
+go :: [Brackets] -> [String] -> IO ()
+go bs [] = getContents >>= goT bs "STDIN"
+go bs fs = mapM_ (goF bs) fs
+
+goF :: [Brackets] -> String -> IO ()
+goF bs f = readFile f >>= goT bs ("File: " ++ f)
+
+goT :: [Brackets] -> String -> String -> IO ()
+goT bs f t = case parse (items bs <* eof) f t
+        of Left  err -> hPrint stderr err >> exitFailure
+           Right res -> putStr $ unlines $ pps res
+
+printHelp :: IO ()
+printHelp = putStrLn "Usage: [STDIN |] prettybraces [-h | --printHelp] [FILE]*"
 
 -- Data:
 
 type Open     = String
 type Close    = String
 type Brackets = (Open, Close)
-data Group    = Group Brackets Items deriving Show
+data Group    = Group Brackets [Item] deriving Show
 type Item     = Either String Group
-type Items    = [Item]
-
--- Constants:
-
-brackets :: [(String, String)]
-brackets = [("(",")"),("{","}"),("[","]"),("<!--","-->")]
-
-bStrings :: [String]
-bStrings = brackets >>= \(l,r) -> [l,r]
 
 -- Parser:
 
-items :: Parsec String Items
-items = many item
+items :: [Brackets] -> Parsec String [Item]
+items bs = many (item bs)
 
-item :: Parsec String Item
-item = choice (map (fmap Right . mkParser) brackets) <|> fmap Left nonBracket
+item :: [Brackets] -> Parsec String Item
+item bs = choice (map (fmap Right . mkParser bs) bs) <|> fmap Left (nonBracket bs)
 
-mkParser :: (String, String) -> Parsec String Group
-mkParser p@(l,r) = do
-  is <- between (string l) (string r) items
+mkParser :: [Brackets] -> (String, String) -> Parsec String Group
+mkParser bs p@(l,r) = do
+  is <- between (string l) (string r) (items bs)
   return $ Group p is
 
-nonBracket :: Parsec String String
-nonBracket = untilP $ choice $ map string bStrings
+nonBracket :: [Brackets] -> Parsec String String
+nonBracket bs = untilP $ choice $ map string bStrings
+  where
+  bStrings = bs >>= \(l,r) -> [l,r]
+
 
 untilP :: MonadParsec s f Char => f a -> f String
 untilP end = some $ notFollowedBy end *> anyChar
@@ -74,10 +99,10 @@ pp' n (Right (Group (l,r) is)) = [blank n l]
                               ++ pps' (succ n) is
                               ++ [blank n r]
 
-pps' :: Int -> Items -> [String]
+pps' :: Int -> [Item] -> [String]
 pps' n = concatMap (pp' n)
 
-pps :: Items -> [String]
+pps :: [Item] -> [String]
 pps = pps' 0
 
 blank :: Int -> String -> String
