87cd3d594fa96bb0ea5a92aa254c568c368ae41c
Lyndon Maydwell <lyndon@sordina.net>
2015-10-20 11:44:13 +1100

    Naming fix

--- src/Main.hs
+++ src/Main.hs
@@ -28,14 +28,14 @@ optsInfo = info (helper <*> opts)
 opts :: Parser (IO ())
 opts = go <$> bracesArg <*> filesArg
 
-bracesArg :: Parser [Brackets]
+bracesArg :: Parser [Braces]
 bracesArg = A.option (bracesArgParser <$> str)
    ( long    "braces"
   <> short   'b'
   <> help    "Space-separated, comma-separated pairs of braces, e.g. '(,) {,} <,>'."
   <> value   [("(",")"),("{","}"),("[","]")])
 
-bracesArgParser :: String -> [Brackets]
+bracesArgParser :: String -> [Braces]
 bracesArgParser = concatMap (pair . splitOn ",") . words
   where
   pair [l,r] = [(l,r)]
@@ -46,14 +46,14 @@ filesArg = A.many (argument str (metavar "FILES..."))
 
 -- Driver:
 
-go :: [Brackets] -> [String] -> IO ()
+go :: [Braces] -> [String] -> IO ()
 go bs [] = getContents >>= goT bs "STDIN"
 go bs fs = mapM_ (goF bs) fs
 
-goF :: [Brackets] -> String -> IO ()
+goF :: [Braces] -> String -> IO ()
 goF bs f = readFile f >>= goT bs ("File: " ++ f)
 
-goT :: [Brackets] -> String -> String -> IO ()
+goT :: [Braces] -> String -> String -> IO ()
 goT bs f t = case parse (items bs <* eof) f t
         of Left  err -> hPrint stderr err >> exitFailure
            Right res -> putStr $ unlines $ pps res
@@ -63,26 +63,26 @@ printHelp = putStrLn "Usage: [STDIN |] prettybraces [-h | --printHelp] [FILE]*"
 
 -- Data:
 
-type Open     = String
-type Close    = String
-type Brackets = (Open, Close)
-data Group    = Group Brackets [Item] deriving Show
-type Item     = Either String Group
+type Open   = String
+type Close  = String
+type Braces = (Open, Close)
+data Group  = Group Braces [Item] deriving Show
+type Item   = Either String Group
 
 -- Parser:
 
-items :: [Brackets] -> Parsec String [Item]
+items :: [Braces] -> Parsec String [Item]
 items bs = many (item bs)
 
-item :: [Brackets] -> Parsec String Item
+item :: [Braces] -> Parsec String Item
 item bs = choice (map (fmap Right . mkParser bs) bs) <|> fmap Left (nonBracket bs)
 
-mkParser :: [Brackets] -> (String, String) -> Parsec String Group
+mkParser :: [Braces] -> (String, String) -> Parsec String Group
 mkParser bs p@(l,r) = do
   is <- between (string l) (string r) (items bs)
   return $ Group p is
 
-nonBracket :: [Brackets] -> Parsec String String
+nonBracket :: [Braces] -> Parsec String String
 nonBracket bs = untilP $ choice $ map string bStrings
   where
   bStrings = bs >>= \(l,r) -> [l,r]
